3a77ac44e0fd7d886380ff0b6afefe50e62a9ea4
Ju <kjj9677@naver.com>
2025-11-22 19:52:16 +0900

    Update style of page header

--- apps/frontend/src/pages/Dashboard/components/Header.tsx
+++ apps/frontend/src/pages/Dashboard/components/Header.tsx
@@ -2,8 +2,8 @@ import Text from '@/components/base/Text'
 
 const Header = () => {
   return (
-    <header className="flex bg-white border-b border-gray-200 justify-center w-full">
-      <div className="flex-col w-[1200px] px-8 py-6">
+    <header className="flex justify-center w-full">
+      <div className="flex-col w-[1200px] px-8 py-4">
         <Text as="h1" type="TITLE" weight="bold" className="text-3xl">
           쇼핑몰 구매 데이터 대시보드
         </Text>
