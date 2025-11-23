9c0bb4ffd1c3b0513e09ceb56986537f590a8aa7
Ju <kjj9677@naver.com>
2025-11-23 09:51:27 +0900

    Update date range using localTimeZone

--- apps/frontend/src/constants/dates.ts
+++ apps/frontend/src/constants/dates.ts
@@ -1,4 +1,4 @@
 export const DATA_DATE_RANGE = {
-  START: new Date('2024-07-01'),
-  END: new Date('2024-07-31'),
+  START: new Date('2024-07-01T00:00:00'),
+  END: new Date('2024-07-31T23:59:59'),
 } as const
